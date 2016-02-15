1af0ca83cf06c5bcba6a5bfc127dd2d99fed9371
williamhickok11 <williamhickok11@gmail.com>
2016-02-15 15:50:44 -0600

    computer died

--- Javascript/augmbfunctions.js
+++ Javascript/augmbfunctions.js
@@ -4,9 +4,9 @@ var Chatty = (function (newChatty){
 	newChatty.userInputToDOM = function (){
 		var chatString = "";
 		chatString += '<div class="row">';
-			chatString += `<div class="col-md-8">${userInput.value}<input type="text" class="userEditInput hidden"></div>`;
-			chatString += '<div class="col-md-2"><button type="button" class="btn btn-info">Edit</button></div>';
-			chatString += '<div class="col-md-2"><button type="button" class="btn btn-danger">Delete</button></div>';
+			chatString += `<div class="col-md-8"><p>${userInput.value}</p><h6>date.now</h6><input type="text" class="userEditInput hidden"></div>`;
+			chatString += '<div class="col-md-2"><button type="button" class="btn btn-info editBtn">Edit</button></div>';
+			chatString += '<div class="col-md-2"><button type="button" class="btn btn-danger deleteBtn">Delete</button></div>';
 		chatString += '</div>';
 		// if chatString > 20 delete top message.
 		document.getElementById("inner-container").innerHTML += chatString
