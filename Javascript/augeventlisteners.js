d7b0aad7c1543fef6faa10b38b3c67724ea0a810
williamhickok11 <williamhickok11@gmail.com>
2016-02-16 12:17:18 -0600

    final functional draft

--- Javascript/augeventlisteners.js
+++ Javascript/augeventlisteners.js
@@ -3,19 +3,23 @@ var Chatty = (function (newChatty) {
 	var userInput = document.getElementById("userInput");
 	var clearBtn = document.getElementById("clearButton");
 
+	//****** LISTEN TO ALL EVENTS ******//
 	newChatty.activateEvents = function(event) {
 
+		//****** Get access to selected user and set it to personValue ******//
 		var personValue = "chattyBot";
 		document.getElementById("feedback-type").addEventListener("change", function(event){
 			personValue = document.getElementById("feedback-type").value
 		})
 		
+		//****** Enter key trigger to put user message to the board ******//
 		userInput.addEventListener("keyup", function(event) {
 			if (event.keyCode === 13) {
 			Chatty.userInputToDOM(personValue);
 			}
 		});
 
+		//****** Listen for click on clear all and removes all elements from inner-container ******//
 		document.getElementById("clearButton").addEventListener("click", function(event) {
 			var myContainer = document.getElementById("inner-container");
 			while (myContainer.firstChild) {
@@ -23,36 +27,42 @@ var Chatty = (function (newChatty) {
 			}
 		});
 
-		document.getElementById("darkTheme").addEventListener("click", function(event) {
+		//****** Listen for theme check-boxes to change value ******//
+		document.getElementById("darkTheme").addEventListener("change", function(event) {
 			document.body.classList.toggle("darkTheme");
 		});
-
-		document.getElementById("largeText").addEventListener("click", function(event) {
+		document.getElementById("largeText").addEventListener("change", function(event) {
 			document.body.classList.toggle("largeText");
 		});
 
+		//****** Listen for click on deleteBtn ******//
 		document.getElementById("inner-container").addEventListener("click", function(event){
-			console.log("click", event.target);
-			console.log(event.target.className.indexOf("deleteBtn"));
+			console.log("listen for deleteBtn", event.target.className.indexOf("deleteBtn"));
+			//****** if innerHTML className exists, run the code. if not, it will return -1 ******//
 			if (event.target.className.indexOf("deleteBtn") >= 0) {
 				event.currentTarget.removeChild(event.target.parentNode.parentNode);
 			}
 		});
 
+		//****** Listen for click on editBtn ******//
 		document.getElementById("inner-container").addEventListener("click", function(event){
 			var eventGrandParent = event.target.parentNode.parentNode.firstChild;
-			console.log("click", event.target);
-			console.log(event.target.className.indexOf("editBtn"));
+			console.log("Listen for editBtn", event.target.className.indexOf("editBtn"));
+			//****** if innerHTML className exists, run the code. if not, it will return -1 ******//
 			if (event.target.className.indexOf("editBtn") >= 0) {
+				//****** Access innerHTML of existing message ******//
 				var editableTextString = eventGrandParent.getElementsByTagName("p")[0].innerHTML;
-				console.log("editableTextString:", editableTextString);
+				//****** Show edit box ******//
 				eventGrandParent.getElementsByClassName("userEditInput")[0].classList.toggle("hidden");
+				//****** Set value of edit text box to the current HTML message ******//
 				eventGrandParent.getElementsByClassName("userEditInput")[0].value = editableTextString;
+				//****** Hide current HTML message and timeStamp ******//
 				eventGrandParent.getElementsByTagName("p")[0].classList.toggle("hidden");
 				eventGrandParent.getElementsByTagName("p")[1].classList.toggle("hidden");
 			}
 		});
 
+		//****** Listen for Enter to Edit The Message ******//
 		document.getElementById("inner-container").addEventListener("keyup", function(event){
 			if (event.keyCode === 13) {
 				var thisTagName = event.target.parentNode.getElementsByTagName("p");
@@ -60,7 +70,8 @@ var Chatty = (function (newChatty) {
 				var hour = today.getHours();
 				var min = today.getMinutes();
 				var timeStamp = hour + ":" + min;
-				
+
+				//****** Reset HTML message and toggle hidden elements ******//
 				thisTagName[0].innerHTML = event.target.value;
 				thisTagName[1].innerHTML = timeStamp;
 				thisTagName[0].classList.toggle("hidden");
--- Javascript/buildDOM.js
+++ Javascript/buildDOM.js
@@ -1,14 +1,17 @@
-// This function's sole purpose is waiting to be triggered by the JSON request to build the DOM
+//*** This function's sole purpose is take the information from JSON and format it ***//
 function buildDOM (mbArray) {
-	console.log("mbArray", mbArray);
 	
+	//****** ACCESS CURRENT DATE ******//
 	var today = new Date();
 	var hour = today.getHours();
 	var min = today.getMinutes();
 	var timeStamp = hour + ":" + min;
 
+	//****** Declare var for build DOM string ******//
 	var chatString = "";
 	var innerDiv = document.getElementById("inner-container");
+	
+	//****** Loop through array and add JSON data ******//
 	for (var i = 0; i < mbArray.length; i++) { 
 		chatString += '<div class="row">';
 			chatString += `<div class="col-md-8"><h3>${mbArray[i].user}:</h3><p>${mbArray[i].text}</p> <p>${timeStamp}</p><input type="text" class="userEditInput hidden" autofocus></div>`;
@@ -16,11 +19,11 @@ function buildDOM (mbArray) {
 			chatString += '<div class="col-md-2"><button type="button" class="btn btn-danger deleteBtn">Delete</button></div>';
 		chatString += '</div>';
 	}
+	//****** Set inner HTML to the created string ******//
 	innerDiv.innerHTML = chatString;
 
 };
 
-
-//Call loadMessages function with buildDOM as the argument//
+//Call loadMessages function with buildDOM as the argument (will initiate the callback [buildDOM()] in messageboard.js)//
 Chatty.loadMessage(buildDOM);
 
